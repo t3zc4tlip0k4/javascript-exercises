8ab7cd408781d14f2d7cb9e2a34adb0d86b6dff2
yunjing23 <cschuster@tenbagroup.com>
2022-07-12 22:22:19 +0300

    Add Solution 01+02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+if(num>1){return string+repeatString(string, num-1);}
+else if(num<0){return "ERROR";}
+else if(num==0){return "";}
+else{return string;}
 };
 
 // Do not edit below this line
