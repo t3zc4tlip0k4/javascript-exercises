e7865c29b900fc19eccf2ef833b0be30f519bdea
yunjing23 <cschuster@tenbagroup.com>
2022-07-13 15:36:44 +0300

    Add Solution 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,others) {
+let arr=arguments[0];
+for(let i=1;i<arguments.length;i++){
+if(arr.indexOf(arguments[i])>=0){arr.splice(arr.indexOf(arguments[i]),1);}
+}
+return arr;
 };
 
 // Do not edit below this line
