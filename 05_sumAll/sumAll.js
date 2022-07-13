8635da25255418adb5ac20152938d1384ca51e3f
yunjing23 <cschuster@tenbagroup.com>
2022-07-13 13:27:04 +0300

    Add Solution 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(int1,int2) {
+let start, end;
+if(int1>int2){
+   start=int2;
+   end=int1;
+}
+else{
+  start=int1;
+  end=int2;
+}
+if (typeof int1!="number" || typeof int2!="number" || int1<0 || int2 <0){
+  return "ERROR";
+}
+else{
+  let sum=0;
+  for(let i=start;i<=end;i++){
+    sum+=i;
+}
+return sum;
+}
 };
 
 // Do not edit below this line
