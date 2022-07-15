ab78dd3d80a5c457f147d580a9e41cf4aa58a9ae
yunjing23 <cschuster@tenbagroup.com>
2022-07-15 17:33:10 +0300

    Add solution 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0){return "OOPS";}
+        let total=[];
+            for(let i=0;i<=num;i++){
+                if(i==0){total.push(0);}
+                else if(i==1 || i==2){total.push(1);}
+                else{//
+                    total.push(total[i-2]+total[i-1]);
+                }
+            }
+    return total[num];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
